cf3345a75c65900ee6c65ce210fa75092cdde3d3
ggobugibugi <275241973+ggobugibugi@users.noreply.github.com>
2026-05-19 01:00:30 +0900

    feat: 20.upwise-edu.github.io upwise.co.kr 커스텀 도메인 연결 (CNAME + astro site URL)

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://upwise-edu.github.io',
+  site: 'https://upwise.co.kr',
   integrations: [mdx(), tailwind()]
 });
